7600c8a82ede9bf037b37ed3a3763a2c906b5154
eilehpo-Irasou <51323756+eilehpo-Irasou@users.noreply.github.com>
2020-09-16 10:40:01 +0200

    Add files via upload

---
+++ seselab-master/exemple.asm
@@ -0,0 +1,35 @@
+
+
+main:
+
+	prn r1
+	prc #10
+	
+	mov r1 #42
+	prn r1
+	
+	prc #10
+
+	mul r2 #3 #3
+	add r3 r1 r2
+	prn r3
+	not r1 #1
+	and r1 r3 r1
+	prc #10
+	prn r1
+	prc #10
+
+	mov @10 #72
+	mov @11 #101
+	mov @12 #108
+	mov @13 #108
+	mov @14 #111
+	prs #10 #6
+
+
+
+	
+	
+
+
+	
